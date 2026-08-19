fececf44b0d5d5c8da4e37a43cb3dec8637d98e9
Malikai <malikairussell@gmail.com>
2026-08-19 15:01:18 -0400

    added ability to go back and forth between main screen and puppy details

--- index.js
+++ index.js
@@ -7,16 +7,45 @@ async function getPlayers() {
   const response = await fetch(API + "/players");
   const data = await response.json();
   const players = data.data.players;
+
   for (let i = 0; i < players.length; i++) {
     const playerCard = document.createElement("div");
     playerCard.classList.add("player-card");
     const playerName = document.createElement("p");
     const playerImage = document.createElement("img");
+    const detailsButton = document.createElement("button");
+    const backButton = document.createElement("button");
     playerImage.src = players[i].imageUrl;
     playerName.textContent = players[i].name;
+    detailsButton.textContent = "Details";
     playerCard.appendChild(playerImage);
     playerCard.appendChild(playerName);
+    playerCard.appendChild(detailsButton);
     app.appendChild(playerCard);
+    detailsButton.addEventListener("click", function () {
+      console.log(players[i].name);
+      app.innerHTML = "";
+      const detailsName = document.createElement("h2");
+      detailsName.textContent = players[i].name;
+      app.appendChild(detailsName);
+      const detailsBreed = document.createElement("p");
+      detailsBreed.textContent = players[i].breed;
+      app.appendChild(detailsBreed);
+      const detailsStatus = document.createElement("p");
+      detailsStatus.textContent = players[i].status;
+      app.appendChild(detailsStatus);
+      const detailsImage = document.createElement("img");
+      detailsImage.classList.add("details-img");
+      detailsImage.src = players[i].imageUrl;
+      app.appendChild(detailsImage);
+      backButton.textContent = "Back to all players.";
+      app.appendChild(backButton);
+      backButton.addEventListener("click", function () {
+        app.innerHTML = "";
+        getPlayers();
+      });
+    });
+
     console.log(players[i].name);
   }
 }
